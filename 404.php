a6f11d38155edb079de9c7a63dfc9a73f633ced4
chloeholles <37389760+chloeholles@users.noreply.github.com>
2018-05-26 12:20:22 -0400

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<section id="content" role="main">
+<article id="post-0" class="post not-found">
+<header class="header">
+<h1 class="entry-title"><?php _e( 'oooof, theres nothing here', 'blank' ); ?></h1>
+</header>
+<section class="entry-content">
+<p><?php _e( 'sadly, this page lacks content', 'blankslate' ); ?></p>
+<?php get_search_form(); ?>
+</section>
+</article>
+</section>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
